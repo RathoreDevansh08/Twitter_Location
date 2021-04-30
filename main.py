75de7bfe8a9d7f608d76bd6d632c168ca9fae9e5
DSR <rathoredevansh08@gmail.com>
2021-04-30 15:58:28 +0530

    db added, page updates

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # imports
 import os
 import pandas as pd
-from flask import Flask, request, send_file, jsonify
+from flask import Flask, request, send_file, jsonify, render_template
 from metadata import MetadataDict, Metadata
+import  mysql.connector
+
+
+# database
+mydb = mysql.connector.connect {
+    host = "sql129.main-hosting.eu",
+    user = "u291509283_cargill"
+    password = "Cargill123",
+    database = "u291509283_cargill"
+}
+
+mycursor = mydb.cursor()
+
+TABLE = "Tweet_data"
 
 
 # server
@@ -13,9 +27,20 @@ app = Flask(__name__)
 
 
 # services
-@app.route('/route', methods=[])
-def request():
+@app.route('/home', methods = [])
+def home():
 
+
+@app.route('/home/<string:city>/', methods = [])
+def location(city):
+
+    mycursor.execute("SELECT * FROM {} WHERE location like {}".format(TABLE, city))
+    myresult = mycursor.fetchall()
+
+    # for x in myresult:
+    #     print(x)
+
+    return render_template("location.html", data = data)
 
 # main
 if __name__ == '__main__':
